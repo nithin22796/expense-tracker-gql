a5de5e06eb2e293e7b6d4708b35fb7b62106626c
Nithin Titus <nithin22796@gmail.com>
2024-09-16 17:51:44 -0500

    Passport configuration

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,47 @@ import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import passport from 'passport';
+import session from 'express-session';
+import connectMongo from 'connect-mongodb-session';
+
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from '@apollo/server/express4'
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { buildContext } from "graphql-passport";
 
 import mergedResolvers from "./resolver/index.js";
 import mergedTypeDefs from "./typeDef/index.js";
 
 import {connectDB} from './db/connectDB.js';
+import { configurePassport } from './passport/passport.config.js';
 
 dotenv.config();
+configurePassport();
+
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions"
+})
+store.on('error', err => console.log);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  },
+  store: store
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -26,9 +55,14 @@ await server.start();
 await connectDB();
  
 app.use(
-  cors(),
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }),
   express.json(),
-  expressMiddleware(server),
+  expressMiddleware(server, {
+    context: async ({req, res}) => buildContext({req, res})
+  }),
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
